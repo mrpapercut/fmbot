df009092132e9db9b2d4bce0dc0168eec935e221
Thom Kaptein <thom@vicompany.nl>
2019-06-05 14:24:04 +0200

    create lastfm class libraries

---
+++ Bot.LastFM/Structs/TokenConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace Bot.LastFM.Structs
+{
+    public struct TokenConfig
+    {
+        /// <summary>
+        /// The key for the Last.FM API.
+        /// </summary>
+        public string LastFMKey;
+
+        /// <summary>
+        /// The secret for the Last.FM API.
+        /// </summary>
+        public string LastFMSecret;
+    }
+}
